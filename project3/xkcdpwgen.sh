84e7f2d8c06ed99e6c2c3b8e51d942215465c3bc
eruuc <132920376+eruuc@users.noreply.github.com>
2023-06-25 22:13:36 -0400

    Update xkcdpwgen.sh

--- project3/xkcdpwgen.sh
+++ project3/xkcdpwgen.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-javac xkcdpwgen.java
-
-if [ $? -ne 0 ]; then
-	echo "Compilation failed."
-	exit 1
+# Check if Java is installed
+if ! command -v java &> /dev/null; then
+    echo "Java is not installed. Please install Java and try again."
+    exit 1
 fi
 
+# Compile the Java program
+javac xkcdpwgen.java
+
+# Check if the compilation was successful
+if [ $? -ne 0 ]; then
+    echo "Compilation failed. Please fix the errors in the Java program and try again."
+    exit 1
+fi
+
+# Run the Java program with command line arguments
 java xkcdpwgen "$@"
